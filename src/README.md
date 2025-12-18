332840a88603e39d5b9a7a8f10935f58776e8f73
Sho Miyazaki <evor14gtc@gmail.com>
2025-12-19 03:12:01 +0900

    3

---
+++ src/README.md
@@ -0,0 +1,1 @@
+# kaitaisinnsho_3
---
+++ src/main/java/com/example/SpringBootSample3Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSample3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSample3Application.class, args);
+	}
+
+}
